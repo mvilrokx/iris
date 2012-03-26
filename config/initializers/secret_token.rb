bf5b4a4aa16d78d05ef549e6ea67e459df0e2e1a
Mark Vilrokx <mvilrokx@gmail.com>
2012-03-26 14:31:23 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iris::Application.config.secret_token = '8bf57226b8db7a5efcd6bf841c875889cc4098e81f638f811fd7ee85a1443805abeae9bdd9fce6da73b10b8f9b8bb7103e2dca2abc6c36a73262e1b058f02b2d'
